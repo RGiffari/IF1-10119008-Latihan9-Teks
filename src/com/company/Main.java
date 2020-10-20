168796d599baa31b4d928e30a0e34f1856eb9898
RGiffari <11R02Giffari@users.noreply.github.com>
2020-10-21 00:00:54 +0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ *
+ * @author
+ * NAMA         : Raihan Giffari
+ * KELAS        : PBO12
+ * NIM          : 10119008
+ * Deskripsi Program    : Program ini berisi tentang penggunaan string
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	char c;
+	char tab = '\t';
+	char newline = '\n';
+	c = 'A';
+	System.out.println("Hasil" + newline + "Char : " + tab + "berisi " + c);
+    }
+}
